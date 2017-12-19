13ae88c9a1ef68ae9feb392e52e5761516afa3e8
Tryggvi <tryggvi.ingason@kentor.se>
2017-12-19 16:02:00 +0100

    Implementing the calculation service

--- btu-calc-service/src/main/kotlin/com/teliacompany/hackathon/birthday/model/CalcResult.kt
+++ btu-calc-service/src/main/kotlin/com/teliacompany/hackathon/birthday/model/CalcResult.kt
@@ -3,4 +3,4 @@ package com.teliacompany.hackathon.birthday.model
 import java.time.LocalDate
 import java.util.concurrent.TimeUnit
 
-data class CalcResult(val timeUnit: TimeUnit = TimeUnit.SECONDS, val number: Long = 0, val dateOfEvent: LocalDate = LocalDate.now())
+data class CalcResult(val timeUnit: TimeUnitBtu = TimeUnitBtu.SECOND, val number: Long = 0, val dateOfEvent: LocalDate = LocalDate.now())
--- btu-calc-service/src/test/kotlin/com/teliacompany/hackathon/birthday/BirthUnitCalculatorTest.kt
+++ btu-calc-service/src/test/kotlin/com/teliacompany/hackathon/birthday/BirthUnitCalculatorTest.kt
@@ -1,16 +1,19 @@
 package com.teliacompany.hackathon.birthday
 
+import com.teliacompany.hackathon.birthday.model.TimeUnitBtu
+import org.junit.Assert
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import java.time.LocalDateTime
+import java.time.Month
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
 class BirthUnitCalculatorTest {
 
     @Test
-    fun contextLoads() {
+    fun getCalc() {
+        val cr = BirthUnitCalculator().getCalc(LocalDateTime.of(2002,Month.JANUARY,1,0,0),
+                LocalDateTime.of(2017,Month.FEBRUARY,1,0,0))
 
+        Assert.assertEquals(TimeUnitBtu.MINUTE, cr.timeUnit);
+        Assert.assertEquals(8000000, cr.number);
     }
 }
