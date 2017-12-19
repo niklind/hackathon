d414e3ddcb8dfc7c4921062f3ac0349a1224fe63
Niklas Lindblad (zdn792) <niklas.lindblad@teliacompany.com>
2017-12-19 09:46:54 +0100

    Initial commit from start.spring.io

---
+++ src/test/kotlin/com/teliacompany/hackathon/birthday/BirthdayApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.teliacompany.hackathon.birthday
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BirthdayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
